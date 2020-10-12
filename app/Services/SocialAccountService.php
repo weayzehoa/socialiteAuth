c8d0fc7e700ec74831dbf4017390ba6b0b92beda
Roger Wu <weayzehoa@gmail.com>
2020-10-12 21:36:35 +0800

    建立 Model 及 修改相關檔案

---
+++ app/Services/SocialAccountService.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Services;
+use App\SocialAccount;
+use App\User;
+use Laravel\Socialite\Contracts\User as ProviderUser;
+use Session;
+
+class SocialAccountService
+{
+    public function createOrGetUser(ProviderUser $providerUser)
+    {
+        $provider = Session::get('provider');
+        $account = SocialAccount::whereProvider($provider)
+            ->whereProviderUserId($providerUser->getId())
+            ->first();
+        if ($account) {
+            return $account->user;
+        } else {
+            $account = new SocialAccount([
+                'provider_user_id' => $providerUser->getId(),
+                'provider' => $provider
+            ]);
+            $user = User::whereEmail($providerUser->getEmail())->first();
+            if (!$user) {
+                $user = User::create([
+                    'email' => $providerUser->getEmail(),
+                    'name' => $providerUser->getName(),
+                    'password' => md5(rand(1,10000)),
+                ]);
+            }
+            $account->user()->associate($user);
+            $account->save();
+            return $user;
+        }
+    }
+}
